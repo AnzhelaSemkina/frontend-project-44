42533d5460b02a88f7b7e572321d8cd807d6960c
AnzhelaSemkina <inforce888@gmail.com>
2022-10-01 22:16:12 +0300

    add game-brain-progression.js

---
+++ games/game-brain-progression.js
@@ -0,0 +1,29 @@
+import readlineSync from 'readline-sync';
+import { greeting, startGameBrain } from '../src/index.js';
+import generateRandomNumber from '../src/generateRandomNumber.js';
+import getResult from '../src/getResult.js';
+
+const name = greeting();
+console.log('What number is missing in the progression?'); // Объясняем правила игры
+
+function startRound() {
+  const array = [];// Создаем пустой массив
+  const randomLength = generateRandomNumber(5, 20) // Генерируем случайную длину <20 && >5
+  const randomIndex = generateRandomNumber(0, (randomLength - 1)); // Генерируем случайный индекс в пределах randomLength - 1
+  const randomNumber = generateRandomNumber(0, 10); // Генерируем случайное число в пределах 10 для шага прогрессии
+  for (let i = 0; array.length <= randomLength; i += randomNumber) {
+    array.push(`${i}`);
+  } // Наполняем массив значениями прогрессии
+  
+  const correctAnswer = array[randomIndex]; // Определяем правильный ответ
+  array[randomIndex] = '..' // Прячем одно случайное значение
+  const progression = array.join(' '); // Переводим массив в строку
+  const question = `Question: ${progression}`;
+  console.log(question); // Задаем вопрос со сгенерированным выражением
+
+  const answer = readlineSync.question('Your answer: '); // Получаем ответ
+
+  return getResult(answer, String(correctAnswer)); //Возвращаем результат
+}
+
+startGameBrain(name, startRound);
