ff42e7c4d185fc2f1ff68289c30c7e7e16dbf2b0
AnzhelaSemkina <inforce888@gmail.com>
2022-10-02 17:04:34 +0300

    fix comments in cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
+console.log('Welcome to the Brain Games!'); // Приветствуем в игре
+const name = readlineSync.question('May I have your name? '); // Спрашиваем имя
+console.log(`Hello, ${name}!`); // Приветствуем по имени
