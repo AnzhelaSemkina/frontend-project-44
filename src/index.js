05068bf8a5481d8855fb919ed1b017070848a4bb
AnzhelaSemkina <inforce888@gmail.com>
2022-10-04 22:33:54 +0300

    refactoring index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-export default (name, runRound) => {
+import readlineSync from 'readline-sync';
+import greet from './cli.js';
+
+export const askQuestion = (number) => `Question: ${number}`;// Задаем вопрос со сгенерированным числом
+
+export const getAnswer = () => {
+  const answer = readlineSync.question('Your answer: '); // Получаем ответ
+  return answer;
+};
+
+export const runGameBrain = (gameRules, runRound) => {
+  const name = greet();
+  console.log(gameRules);
   for (let i = 0; i < 3; i += 1) { // Запускаем цикл на 3 итерации
     const resultRound = runRound();
     if (!resultRound) { // Если раунд завершился неправильным ответом
