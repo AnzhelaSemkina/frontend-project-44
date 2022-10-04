87c468f8f90f2038400910807a5b83bad8a06c14
AnzhelaSemkina <inforce888@gmail.com>
2022-10-04 22:33:21 +0300

    refactoring brain-prime.js

--- src/games/game-brain-prime.js
+++ src/games/game-brain-prime.js
@@ -1,11 +1,6 @@
-import readlineSync from 'readline-sync';
-import greet from '../cli.js';
-import runGameBrain from '../index.js';
+import { askQuestion, getAnswer, runGameBrain } from '../index.js';
 import generateRandomNumber from '../generateRandomNumber.js';
 import getResult from '../getResult.js';
-
-const name = greet(); // Запускаем импортированное приветствие
-console.log('Answer "yes" if the number is even, otherwise answer "no".'); // Объясняем правила игры
 
 const isPrime = (number) => { // Создаем функцию проверки числа
   if (number === 2) { // Если это 2, то возвращаем true
@@ -22,15 +17,15 @@ const isPrime = (number) => { // Создаем функцию проверки 
   } return true; // то возвращаем true
 };
 
-const startRound = () => { // Функция 1 раунда игры
-  const randomNumber = generateRandomNumber(0, 100); // Генерируем  1 случайное число в пределах 100
-  const question = `Question: ${randomNumber}`;
-  console.log(question); // Задаем вопрос со сгенерированным числом
+export default () => {
+  const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".'; // Объясняем правила игры
+  const startRound = () => { // Функция 1 раунда игры
+    const randomNumber = generateRandomNumber(0, 100); // Генерируем случайное число в пределах 100
+    console.log(askQuestion(randomNumber)); // Задаем вопрос со сгенерированным числом
+    const userAnswer = getAnswer(); // Получаем ответ
+    const isCorrectAnswer = isPrime(randomNumber) ? 'yes' : 'no'; // Определяем правильный ответ
+    return getResult(userAnswer, isCorrectAnswer); // Определяем результат раунда
+  };
 
-  const answer = readlineSync.question('Your answer: '); // Получаем ответ
-
-  const isCorrectAnswer = isPrime(randomNumber) ? 'yes' : 'no'; // Определяем правильный ответ
-  return getResult(answer, isCorrectAnswer); // Определяем результат раунда
+  runGameBrain(gameRules, startRound);
 };
-
-runGameBrain(name, startRound);
