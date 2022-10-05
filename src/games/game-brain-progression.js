b6dba709b89a8e50923bceca1cd052b99d360520
AnzhelaSemkina <inforce888@gmail.com>
2022-10-05 18:38:58 +0300

    refactoring game-brain-progression.js

--- src/games/game-brain-progression.js
+++ src/games/game-brain-progression.js
@@ -1,30 +1,31 @@
-import readlineSync from 'readline-sync';
-import greet from '../cli.js';
-import runGameBrain from '../index.js';
+import { askQuestion, getAnswer, runGameBrain } from '../index.js';
 import generateRandomNumber from '../generateRandomNumber.js';
 import getResult from '../getResult.js';
 
-const name = greet(); // Запускаем импортированное приветствие
-console.log('What number is missing in the progression?'); // Объясняем правила игры
-
-const runRound = () => { // Функция 1 раунда
+const getProgression = (length, stepProgression) => {
   const array = [];// Создаем пустой массив
-  const randomLength = generateRandomNumber(5, 20); // Генерируем случайную длину <20 && >5
-  const randomIndex = generateRandomNumber(0, (randomLength - 1)); // Генерируем случайный индекс
-  const randomNumber = generateRandomNumber(0, 10); // Генерируем число для шага прогрессии
-  for (let i = 0; array.length <= randomLength; i += randomNumber) {
+  for (let i = 0; array.length <= length; i += stepProgression) {
     array.push(`${i}`);
   } // Наполняем массив значениями прогрессии
-
-  const correctAnswer = array[randomIndex]; // Определяем правильный ответ
-  array[randomIndex] = '..'; // Прячем одно случайное значение
-  const progression = array.join(' '); // Переводим массив в строку
-  const question = `Question: ${progression}`;
-  console.log(question); // Задаем вопрос со сгенерированным выражением
-
-  const answer = readlineSync.question('Your answer: '); // Получаем ответ
-
-  return getResult(answer, String(correctAnswer)); // Возвращаем результат
+  return array;
 };
 
-runGameBrain(name, runRound);
+export default () => {
+  const gameRules = 'What number is missing in the progression?'; // Объясняем правила игры
+  const runRound = () => { // Функция 1 раунда
+    const randomLength = generateRandomNumber(5, 20); // Генерируем случайную длину <20 && >5
+    const randomIndex = generateRandomNumber(0, (randomLength - 1)); // Генерируем случайный индекс
+    const randomStep = generateRandomNumber(0, 10); // Генерируем число для шага прогрессии
+    const array = getProgression(randomLength, randomStep); // Получаем массив чисел
+    const correctAnswer = array[randomIndex]; // Определяем правильный ответ
+    array[randomIndex] = '..'; // Прячем это случайное значение
+    const progression = array.join(' '); // Переводим массив в строку
+
+    console.log(askQuestion(progression)); // Задаем вопрос со сгенерированным выражением
+    const userAnswer = getAnswer(); // Получаем ответ
+
+    return getResult(userAnswer, String(correctAnswer)); // Возвращаем результат
+  };
+
+  runGameBrain(gameRules, runRound);
+};
