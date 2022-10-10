1dcd9e68bd94b3551813f15d4a4dd2a9d2d92e3b
AnzhelaSemkina <inforce888@gmail.com>
2022-10-10 20:44:24 +0300

    refactoring brain-gcd.js

--- src/games/game-brain-gcd.js
+++ src/games/game-brain-gcd.js
@@ -1,6 +1,5 @@
-import { askQuestion, getAnswer, runGameBrain } from '../index.js';
+import { runGameBrain } from '../index.js';
 import generateRandomNumber from '../generateRandomNumber.js';
-import getResult from '../getResult.js';
 
 const getGreatestCommonDivisor = (num1, num2) => { // Функция нахождения Наибольшего Общего Делителя
   let number1 = num1;
@@ -21,18 +20,16 @@ const getRemainder = (num1, num2) => {
   return getGreatestCommonDivisor(num2, num1); // Иначе возврат результата поменяв местами аругменты
 };
 
+const gameRules = 'Find the greatest common divisor of given numbers.'; // Объясняем правила игры
+
 export default () => {
-  const gameRules = 'Find the greatest common divisor of given numbers.'; // Объясняем правила игры
   const runRound = () => { // Функция 1 раунда
     const randomNumber1 = generateRandomNumber(1, 25); // Генерируем  1случайное число в пределах 25
     const randomNumber2 = generateRandomNumber(1, 50); // Генерируем  2cлучайное число в пределах 50
-    const pairOfNumber = `${randomNumber1} ${randomNumber2}`;
-
-    console.log(askQuestion(pairOfNumber)); // Задаем вопрос
-    const userAnswer = getAnswer(); // Получаем ответ
-
-    return getResult(userAnswer, String(getRemainder(randomNumber1, randomNumber2)));
-    // Возвращаем результат сравнения ответа пользователя с правильным ответом
+    const pairOfNumber = `${randomNumber1} ${randomNumber2}`; // Записываем пару чисел для вопроса
+    // Создаем массив для правильной работы логики
+    const result = [pairOfNumber, getRemainder(randomNumber1, randomNumber2)];
+    return result;
   };
 
   runGameBrain(gameRules, runRound);
