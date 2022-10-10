cc7573d4578c5b89bde61fe4c8015d682ad02789
AnzhelaSemkina <inforce888@gmail.com>
2022-10-10 20:42:51 +0300

    refactoring brain-even.js

--- src/games/game-brain-even.js
+++ src/games/game-brain-even.js
@@ -1,17 +1,16 @@
-import { askQuestion, getAnswer, runGameBrain } from '../index.js';
+import { runGameBrain } from '../index.js';
 import generateRandomNumber from '../generateRandomNumber.js';
-import getResult from '../getResult.js';
+
+const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no'); // Определяем четность числа
+
+const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".'; // Объясняем правила игры
 
 export default () => {
-  const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".'; // Объясняем правила игры
   const runRound = () => { // Функция 1 раунда
     const randomNumber = generateRandomNumber(0, 100); // Генерируем случайное число в пределах 100
-
-    console.log(askQuestion(randomNumber)); // Задаем вопрос
-    const userAnswer = getAnswer(); // Получаем ответ
-
-    const isCorrectAnswer = randomNumber % 2 === 0 ? 'yes' : 'no'; // Определяем правильный ответ
-    return getResult(userAnswer, isCorrectAnswer); // Возвращаем результат раунда
+    const isCorrectAnswer = isEven(randomNumber); // Определяем правильный ответ
+    const result = [randomNumber, isCorrectAnswer]; // Создаем массив для правильной работы логики
+    return result;
   };
 
   runGameBrain(gameRules, runRound);
