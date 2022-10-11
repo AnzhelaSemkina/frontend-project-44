cad8958d5909a02f6123075630dc6296f1f9b038
AnzhelaSemkina <inforce888@gmail.com>
2022-10-11 22:59:12 +0300

    update src/generateRandomNumber.js

--- src/generateRandomNumber.js
+++ src/generateRandomNumber.js
@@ -1,4 +1,4 @@
 // Функция генерации  случайного числа от min до max
-const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const generateRandomNumber = (max, min = 1) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export default generateRandomNumber;
