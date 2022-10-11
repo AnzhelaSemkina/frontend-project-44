f49fa4caba3c89edfda89db5c7e39efa07a67c34
AnzhelaSemkina <inforce888@gmail.com>
2022-10-11 23:04:13 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import readlineSync from 'readline-sync';
-import greet from './cli.js';
-import getResult from './getResult.js';
+
+const greet = () => {
+  console.log('Welcome to the Brain Games!'); // Приветствуем в игре
+  const name = readlineSync.question('May I have your name? '); // Узнаем имя
+  console.log(`Hello, ${name}!`); // Приветствуем игрока
+  return name;
+};
 
 export const askQuestion = (number) => `Question: ${number}`;// Задаем вопрос со сгенерированным числом
 
@@ -9,11 +14,23 @@ export const getAnswer = () => {
   return answer;
 };
 
-export const runGameBrain = (gameRules, getNumbers) => {
+const getResult = (userAnswer, correctAnswer) => {
+  if (userAnswer === correctAnswer) { // Сравниваем ответы
+    console.log('Correct!'); // Сообщаем о выигрыше
+    return true; // Возвращаем результат
+  }
+  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`); // Сообщаем о проигрыше
+
+  return false; // Возвращаем результат
+};
+
+const amountRound = 3; // Максимальное количество раундов игры
+
+export const runGameBrain = (gameRules, getValuesRound) => {
   const name = greet();
   console.log(gameRules);
-  for (let i = 0; i < 3; i += 1) { // Запускаем цикл на 3 итерации
-    const [question, correctAnswer] = getNumbers();
+  for (let i = 0; i < amountRound; i += 1) { // Запускаем цикл на 3 раунда
+    const [question, correctAnswer] = getValuesRound();
     console.log(askQuestion(question)); // Задаем вопрос со сгенерированным выражением
     const userAnswer = getAnswer(); // Получаем ответ
     const resultRound = getResult(userAnswer, String(correctAnswer));
