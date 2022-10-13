ecff45a7f16a1097776807be06a2e138340187d6
AnzhelaSemkina <inforce888@gmail.com>
2022-10-13 21:43:24 +0300

    refactoring index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import readlineSync from 'readline-sync';
 
 const askQuestion = (number) => `Question: ${number}`;// Задаем вопрос со сгенерированным числом
 
-const getAnswer = () => {
-  const answer = readlineSync.question('Your answer: '); // Получаем ответ
-  return answer;
-};
-
 const getResult = (userAnswer, correctAnswer) => {
   if (userAnswer === correctAnswer) { // Сравниваем ответы
     console.log('Correct!'); // Сообщаем о выигрыше
@@ -27,8 +22,8 @@ const runGameBrain = (gameRules, getValuesRound) => {
   for (let i = 0; i < amountRound; i += 1) { // Запускаем цикл на 3 раунда
     const [question, correctAnswer] = getValuesRound();
     console.log(askQuestion(question)); // Задаем вопрос со сгенерированным выражением
-    const userAnswer = getAnswer(); // Получаем ответ
-    const resultRound = getResult(userAnswer, String(correctAnswer));
+    const answer = readlineSync.question('Your answer: '); // Получаем ответ
+    const resultRound = getResult(answer, String(correctAnswer));
     if (!resultRound) { // Если раунд завершился неправильным ответом
       console.log(`Let's try again, ${name}!`); // Говорим об этом
       return; // Заканчиваем выполнение функции
